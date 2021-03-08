617c7ad29917aaee32df9ce490363f1ba932cdda
歐川銘 <tsw303005@ouchuandiaode-MacBook-Pro.local>
2021-03-09 00:02:09 +0800

    first init

---
+++ 160. Intersection of Two Linked Lists/function.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int countA = 0;
+        int countB = 0;
+        
+        ListNode* cur = headA;
+        ListNode* com;
+        while (cur != NULL) {
+            countA += 1;
+            cur = cur->next;
+        }
+        cur = headB;
+        while (cur != NULL) {
+            countB += 1;
+            cur = cur->next;
+        }
+        int run = 0;
+        if (countA > countB) {
+            run = countA - countB;
+            cur = headA;
+            com = headB;
+            for (int i = 0; i < run; i++) {
+                cur = cur->next;
+            }
+        } else if (countB > countA) {
+            run = countB - countA;
+            cur = headB;
+            com = headA;
+            for (int i = 0; i < run; i++) {
+                cur = cur->next;
+            }
+        } else {
+            cur = headA;
+            com = headB;
+        }
+        
+        while (cur != com) {
+            cur = cur->next;
+            com = com->next;
+        }
+        
+        if (cur == NULL) {
+            return NULL;
+        } else {
+            return cur;
+        }
+    }
+};
---
+++ 5. Longest Palindromic Substring/function.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int length = s.length();
+        int left, right, max;
+        int now, run;
+        string ans = "";
+        
+        
+        now = -1;
+        max = -1;
+        left = right = 0;
+        for (int i = 0; i < 2*length-1; i++) {
+            if (i % 2 == 0) {
+                now += 1;
+                run = 1;
+                while (now-run >= 0 && now + run < length) {
+                    if (s[now-run] == s[now+run]) {
+                        if (2*run+1 > max) {
+                            max = 2*run + 1;
+                            left = now-run;
+                            right = now+run;
+                        }
+                        run += 1;
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                run = 0;
+                while (now-run >= 0 && now+run+1 < length) {
+                    if (s[now-run] == s[now+run+1]) {
+                        if (2*run+2 > max) {
+                            max = 2*run+2;
+                            left = now-run;
+                            right = now+run+1;
+                        }
+                        run += 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        for (int i = left; i <= right; i++) {
+            ans += s[i];
+        }
+        return ans;
+    }
+};
