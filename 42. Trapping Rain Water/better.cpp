d23a6ec6278c86a45d842ad253c500a9c6d417e7
tsw303005 <tsw303005@gmail.com>
2022-09-18 15:12:18 +0800

    update 42 solution explain

--- 42. Trapping Rain Water/better.cpp
+++ 42. Trapping Rain Water/better.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <vector>
+
+/*
+    you can think this problem has left, and right bound,
+    every time we compare heigh[left] and height[right] to find which is lower (tmp),
+    the lower one will compare to the lower value now,
+    if tmp < lower means that the tmp is trapping and the ans will be added (lower - tmp),
+    if tmp > loewr means that the new bound is found, so the lower value will changed to the tmp,
+    continue to do this until the left >= right,
+
+    so the idea is that we need to find the left and right bound if we want to know if index i is trapping,
+    we have to find if there are left and right value beigger than index[i],
+    the we can get the ans
+*/
 
 using namespace std;
 
