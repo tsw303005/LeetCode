625bb060fbf3890d2c00bccbf6ce4bf7cd500f68
tsw303005 <tsw303005@gmail.com>
2022-09-23 12:12:36 +0800

    add 1680

---
+++ 1680. Concatenation of Consecutive Binary Numbers/main.cpp
@@ -0,0 +1,16 @@
+
+using namespace std;
+
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        long ans = 0, len = 0, mod = 1e9+7;
+        
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) len++;
+            ans = ((ans << len) % mod + i) % mod;
+        }
+        
+        return ans;
+    }
+};
