3d1f59142ce78aacfa83d9a5f4cc7df3265b9e9e
歐川銘 <ivan@ouchuanmings-Laptop.local>
2023-10-30 15:24:34 +0800

    add 458

---
+++ 458. Poor Pigs/soluction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int pigs = 0;
+        
+        while (pow(minutesToTest / minutesToDie + 1, pigs) < buckets) pigs++;
+        
+        return pigs;
+    }
+};
